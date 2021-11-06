05ad20b8fa6bfee455eff2deb827f476bbb89ae7
Jhonny Sánchez <jhonny.sanchez@valudio.com>
2021-11-06 11:31:59 +0100

    Remove footer and disable alert

--- components/alert.js
+++ components/alert.js
@@ -10,9 +10,9 @@ export default function Alert({ preview }) {
         'bg-accent-1 border-accent-2': !preview,
       })}
     >
-      <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
+      {preview &&
+        <Container>
+          <div className="py-2 text-center text-sm">
             <>
               This page is a preview.{' '}
               <a
@@ -23,20 +23,10 @@ export default function Alert({ preview }) {
               </a>{' '}
               to exit preview mode.
             </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
-      </Container>
+            )
+          </div>
+        </Container>
+      }
     </div>
   )
 }
