fe866903959efbd2cae697dc1becc4efaf657288
Jhonny Sánchez <jhonny.sanchez@koahealth.com>
2021-11-07 13:21:08 +0100

    get home info from cms

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { indexQuery } from '../lib/queries'
-import { getClient, overlayDrafts } from '../lib/sanity.server'
+import Head from "next/head";
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { homeQuery, indexQuery } from "../lib/queries";
+import { getClient, overlayDrafts } from "../lib/sanity.server";
 
-export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index({ allPosts, preview, home }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Centro educativo</title>
+          <title>{home?.title}</title>
         </Head>
         <Container>
-          <Intro />
-          {heroPost && (
+          <Intro title={home?.title} subtitle={home?.subtitle} mainImage={home?.mainImage}/>
+          {/* {heroPost && (
             <HeroPost
               title={heroPost.title}
               coverImage={heroPost.coverImage}
@@ -27,17 +27,24 @@ export default function Index({ allPosts, preview }) {
               slug={heroPost.slug}
               excerpt={heroPost.excerpt}
             />
-          )}
+          )} */}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
+  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery));
+
+  const home = await getClient(preview).fetch(
+    homeQuery
+  );
+
+  console.log(home);
+
   return {
-    props: { allPosts, preview },
-  }
+    props: { allPosts, preview, home },
+  };
 }
