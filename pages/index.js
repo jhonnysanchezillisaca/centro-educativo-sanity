bba3d9789687cf191bf37cc437969704c5007308
Jhonny Sánchez <jhonny.sanchez@firstvet.com>
2024-10-13 16:02:17 +0200

    small style fixes on index and md content

--- pages/index.js
+++ pages/index.js
@@ -25,15 +25,15 @@ export default function Index({ allPosts, preview, home, notification }) {
             mainImage={home?.mainImage}
           />
           <div className="max-w-5xl mx-auto mb-12">
-            <h3 className="text-3xl md:text-5xl font-semibold tracking-tight leading-tight">
+            <h3 className="text-4xl md:text-5xl font-semibold tracking-tight leading-tight underline">
               {home?.section1Title}
             </h3>
             <PostBody content={home?.section1Content} />
-            <h3 className="text-3xl md:text-5xl font-semibold tracking-tight leading-tight">
+            <h3 className="text-4xl md:text-5xl font-semibold tracking-tight leading-tight mt-10 underline">
               {home?.section2Title}
             </h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6 md:mb-12">
-              <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-6 md:mb-12">
+              <div className="flex flex-col justify-between ">
                 <PostBody content={home?.section2Content1} />
                 <Image
                   width="300"
@@ -42,7 +42,7 @@ export default function Index({ allPosts, preview, home, notification }) {
                   src={urlForImage(home?.section2Image1).url()}
                 />
               </div>
-              <div>
+              <div className="flex flex-col justify-between">
                 <PostBody content={home?.section2Content2} />
                 <Image
                   width="300"
@@ -51,7 +51,7 @@ export default function Index({ allPosts, preview, home, notification }) {
                   src={urlForImage(home?.section2Image2).url()}
                 />
               </div>
-              <div>
+              <div className="flex flex-col justify-between">
                 <PostBody content={home?.section2Content3} />
                 <Image
                   width="300"
@@ -63,7 +63,7 @@ export default function Index({ allPosts, preview, home, notification }) {
               {
                 home?.section2Image4 && home?.section2Content4 
                 ? 
-                <div>
+                <div className="flex flex-col justify-between">
                   <PostBody content={home?.section2Content4} />
                   <Image
                     width="300"
@@ -76,7 +76,7 @@ export default function Index({ allPosts, preview, home, notification }) {
               }
             </div>
 
-            <h3 className="text-3xl md:text-5xl font-semibold tracking-tight leading-tight">
+            <h3 className="text-4xl md:text-5xl font-semibold tracking-tight leading-tight mt-10 underline">
               {home?.section3Title}
             </h3>
             <div className="grid grid-cols-1 md:grid-cols-2 mb-6 md:mb-12">
@@ -90,31 +90,21 @@ export default function Index({ allPosts, preview, home, notification }) {
                 />
               </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div>
-                <h3 className="text-3xl md:text-5xl font-semibold tracking-tight leading-tight mb-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-0">
+              <div className="flex flex-col items-center">
+                <h3 className="text-4xl md:text-5xl font-semibold tracking-tight leading-tight mb-4 underline">
                   {home?.section4Title}
                 </h3>
                 <PostBody content={home?.section4Content} />
               </div>
-              <div>
-                <h3 className="text-3xl md:text-5xl font-semibold tracking-tight leading-tight mb-4">
+              <div className="flex flex-col items-center">
+                <h3 className="text-4xl md:text-5xl font-semibold tracking-tight leading-tight mb-4 underline">
                   {home?.section5Title}
                 </h3>
                 <PostBody content={home?.section5Content} />
               </div>
             </div>
           </div>
-          {/* {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )} */}
           {allPosts.length > 0 && <MoreStories posts={allPosts} />}
         </Container>
       </Layout>
