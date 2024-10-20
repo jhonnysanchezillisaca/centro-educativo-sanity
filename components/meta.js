f41af0b096867592f5df15c1fb60234458f62081
Jhonny Sánchez <jhonny.sanchez@firstvet.com>
2024-10-20 10:47:04 +0200

    update og image

--- components/meta.js
+++ components/meta.js
@@ -21,11 +21,6 @@ export default function Meta() {
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
@@ -33,9 +28,9 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`Web del Centre Educatiu bahá’í al sud de Sabadell`}
+        content={`Web del Institut Bahá'í al sud de Sabadell`}
       />
-      <meta property="og:image" content="https://cedu.bahaisabadell.org/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fdlz5307s%2Fproduction%2F19021aea2e1eac1a7900f65bf8232e9c3c823e7a-4032x3024.jpg%3Frect%3D0%2C454%2C4032%2C2016%26w%3D2000%26h%3D1000%26fit%3Dmax%26auto%3Dformat&w=2048&q=75" key="ogImage" />
+      <meta property="og:image" content="https://www.bahaisabadell.org/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fdlz5307s%2Fproduction%2F3136643c4e49b6f77c2538d083e6aaa461af3634-1702x630.png%3Frect%3D221%2C0%2C1260%2C630%26w%3D2000%26h%3D1000%26fit%3Dmax%26auto%3Dformat&w=2048&q=90" key="ogImage" />
     </Head>
   )
 }
